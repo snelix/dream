a229e21357198c1ee17e78f4470bf0c6a74bd6e4
Sergey <sergonix.w.y.s@gmail.com>
2022-01-29 09:56:26 +0700

    start

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-btn-prev',
+    },
+  });
