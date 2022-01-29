93612fa1b662d45db717bdf5a9ca05efa489c328
Sergey <sergonix.w.y.s@gmail.com>
2022-01-29 18:45:05 +0700

    all section done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.header__slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -8,4 +8,29 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-btn-next',
       prevEl: '.swiper-btn-prev',
     },
+  });
+
+  const swiperProperty = new Swiper(".property__swiper", {
+    slidesPerView: 3.4,
+    spaceBetween: 32,
+    pagination: {
+      clickable: true,
+    },
+  });
+
+  const swiperFeedback = new Swiper('.feedback__slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-btn-prev',
+    },
+
   });
