9af96a6d23f8d4141ecc0518a190e1fdf703a8cf
Sergey <sergonix.w.y.s@gmail.com>
2022-02-02 17:54:18 +0700

    adaptation

--- js/script.js
+++ js/script.js
@@ -12,10 +12,29 @@ const swiper = new Swiper('.header__slider', {
 
   const swiperProperty = new Swiper(".property__swiper", {
     slidesPerView: 3.4,
-    spaceBetween: 32,
     pagination: {
       clickable: true,
     },
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
+      },
+      577: {
+        slidesPerView: 1.6,
+      },
+      769: {
+        slidesPerView: 2.1,
+      },
+      993: {
+        slidesPerView: 2.5,
+      },
+      1201: {
+        slidesPerView: 3.2,
+      },
+      1457: {
+        slidesPerView: 3.4,
+      }
+    }
   });
 
   const swiperFeedback = new Swiper('.feedback__slider', {
